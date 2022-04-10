926b944204073fcc9e63a6dc0e79b25f3458300b
luckyantony <83889059+luckyantony@users.noreply.github.com>
2022-04-11 01:09:17 +0300

    corrections

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,20 @@
-# Print Pascal's Triangle in Python
+#!/usr/bin/python3
 from math import factorial
  
 # input n
 n = 5
-for i in range(n):
-    for j in range(n-i+1):
  
-        # for left spacing
-        print(end=" ")
+for i in range(1, n+1):
+    for j in range(0, n-i+1):
+        print(' ', end='')
  
-    for j in range(i+1):
+    # first element is always 1
+    C = 1
+    for j in range(1, i+1):
  
-        # nCr = n!/((n-r)!*r!)
-        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+        # first value in a line is always 1
+        print(' ', C, sep='', end='')
  
-    # for new line
+        # using Binomial Coefficient
+        C = C * (i - j) // j
     print()

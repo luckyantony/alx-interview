dd2061e364a530a430f9ac8e67e3c83ca4b0bef1
luckyantony <83889059+luckyantony@users.noreply.github.com>
2022-04-11 01:16:02 +0300

    modification

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-from math import factorial
- 
-# input n
-n = 5
- 
-for i in range(1, n+1):
-    for j in range(0, n-i+1):
-        print(' ', end='')
- 
-    # first element is always 1
-    C = 1
-    for j in range(1, i+1):
- 
-        # first value in a line is always 1
-        print(' ', C, sep='', end='')
- 
-        # using Binomial Coefficient
-        C = C * (i - j) // j
-    print()
+""" Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """ Returns a list of lists of integers
+        representing the Pascal's triangle of n
+    """
+    lis = []
+    if n > 0:
+        for i in range(1, n + 1):
+            temp = []
+            integer = 1
+            for j in range(1, i + 1):
+                temp.append(integer)
+                integer = integer * (i - j) // j
+            lis.append(temp)
+    return lis
